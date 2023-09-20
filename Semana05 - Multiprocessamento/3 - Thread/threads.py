4902073e555b7f77a5335412e36cb9eba0b8bb38
DouglasSilvaC <136767228+DouglasSilvaC@users.noreply.github.com>
2023-09-20 20:40:06 -0300

    Add files via upload

---
+++ Semana05 - Multiprocessamento/3 - Thread/threads.py
@@ -0,0 +1,26 @@
+//SEII
+//Threads
+
+from threading import Thread
+import os
+import math
+
+
+def calc():
+    for i in range(0, 4000000):
+        math.sqrt(i)
+
+
+threads = []
+
+for i in range(os.cpu_count()):
+    print("registering threads %d" % i)
+    threads.append(Thread(target=calc))
+
+
+for thread in threads:
+    thread.start()
+
+
+for thread in threads:
+    thread.join()
